b5dc8e18a2f4d32c41d9db93e64e2e8aa57dfd8a
rafaellucio <rafaelantoniolucio@gmail.com>
2013-11-06 15:01:37 -0200

    corrected loginBar and index.php

--- phpProject/includes/loginBar.php
+++ phpProject/includes/loginBar.php
@@ -6,13 +6,19 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="brand" href="#">Project Moodle</a>
+				<a class="brand" href="index.php">Project Moodle</a>
 				<div class="nav-collapse collapse">
 					<ul class="nav">
 						<li class="active"><a href="#">Home</a></li>
 						<li><a href="#about">Sobre</a></li>
 						<li><a href="#contact">Contato</a></li>
-						<li><a href="#name">Nome do usuário</a></li>
+						<li>
+							<a href="#name">
+							<?php 
+								echo $_SESSION['username'];
+							?>
+							</a>
+						</li>
 						<li><a href="#img">IMG</a></li>
 						<li><a href="../functions/session_logout.php">Logout</a></li>
 					</ul>
